b249d860c9cec444c4832b35569eeec06f646760
IoriBranford <ioribranford@gmail.com>
2016-06-07 17:16:04 -0700

    Start test level.

---
+++ imp.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="imp" tilewidth="64" tileheight="64" tilecount="3" columns="3">
+ <properties>
+  <property name="credit" value="TsarCube http://tsarcube.deviantart.com/ (CC BY 3.0)"/>
+ </properties>
+ <image source="imp.png" width="192" height="64"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="4" x="24" y="24" width="16" height="16">
+    <properties>
+     <property name="category" value="Player"/>
+     <property name="mask" value="PlayerShot"/>
+    </properties>
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="125"/>
+   <frame tileid="1" duration="125"/>
+   <frame tileid="2" duration="125"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ impshot.tsx
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="impshot" tilewidth="16" tileheight="32" tilecount="2" columns="2">
+ <properties>
+  <property name="credit" value="Master484 http://m484games.ucoz.com/ (CC0 1.0)"/>
+ </properties>
+ <image source="impshot.png" width="32" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="8" width="16" height="16">
+    <properties>
+     <property name="category" value="PlayerShot"/>
+    </properties>
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="64"/>
+   <frame tileid="1" duration="64"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="8" width="16" height="16">
+    <properties>
+     <property name="category" value="PlayerShot"/>
+    </properties>
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="1" duration="64"/>
+   <frame tileid="0" duration="64"/>
+  </animation>
+ </tile>
+</tileset>
