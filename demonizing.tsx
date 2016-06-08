a9024adaa07b80da524a0c410e2588ffb3bb059b
IoriBranford <ioribranford@gmail.com>
2016-06-07 23:48:04 -0700

    Test demonization effect.

---
+++ demonizing.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="demonizing" tilewidth="64" tileheight="64" tilecount="3" columns="3">
+ <image source="demonizing.png" width="192" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="250"/>
+   <frame tileid="1" duration="250"/>
+   <frame tileid="2" duration="1500"/>
+  </animation>
+ </tile>
+</tileset>
