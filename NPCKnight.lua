313c7f6440b30dc6f20b0361a1fbbf0856617f52
IoriBranford <ioribranford@gmail.com>
2016-06-20 13:58:58 -0700

    Knight enemy script.

--- NPCKnight.lua
+++ NPCKnight.lua
@@ -4,16 +4,16 @@ local ShmupNPC = levity.machine:requireScript("ShmupNPC")
 local ShmupBullet = levity.machine:requireScript("ShmupBullet")
 require "class"
 
-local NPCArcher = class(ShmupNPC, function(self, id)
+local NPCKnight = class(ShmupNPC, function(self, id)
 	ShmupNPC.init(self, id)
 	self.firetimer = 0
-	self.health = 4
+	self.health = 8
 end)
 
-NPCArcher.BulletSpeed = 4*60
-NPCArcher.BulletInterval = 1
+NPCKnight.BulletSpeed = 2*60
+NPCKnight.BulletInterval = .5
 
-function NPCArcher:beginMove(dt)
+function NPCKnight:beginMove(dt)
 	ShmupNPC.beginMove(self, dt)
 	if not self.object.visible then
 		return
@@ -35,20 +35,20 @@ function NPCArcher:beginMove(dt)
 	end
 
 	self.firetimer = self.firetimer + dt
-	if self.firetimer >= NPCArcher.BulletInterval then
-		while self.firetimer >= NPCArcher.BulletInterval do
-			self.firetimer = self.firetimer - NPCArcher.BulletInterval
+	if self.firetimer >= NPCKnight.BulletInterval then
+		while self.firetimer >= NPCKnight.BulletInterval do
+			self.firetimer = self.firetimer - NPCKnight.BulletInterval
 		end
 
-		local angle = math.atan2(playerdy, playerdx)
+		local angle = math.pi * .5 --math.atan2(playerdy, playerdx)
 
-		cx = cx + (math.cos(angle) * NPCArcher.BulletSpeed * self.firetimer)
-		cy = cy + (math.sin(angle) * NPCArcher.BulletSpeed * self.firetimer)
+		cx = cx + (math.cos(angle) * NPCKnight.BulletSpeed * self.firetimer)
+		cy = cy + (math.sin(angle) * NPCKnight.BulletSpeed * self.firetimer)
 
-		ShmupBullet.create(cx, cy, NPCArcher.BulletSpeed, angle,
-			"archershot", 0, ShmupNPC.ShotLayer,
+		ShmupBullet.create(cx, cy, NPCKnight.BulletSpeed, angle,
+			"knightshot", 0, ShmupNPC.ShotLayer,
 			ShmupCollision.Category_NPCShot)
 	end
 end
 
-return NPCArcher
+return NPCKnight
---
+++ knightshot.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="knightshot" tilewidth="16" tileheight="32" tilecount="1" columns="1">
+ <tileoffset x="-8" y="16"/>
+ <image source="knightshot.png" width="16" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="2" x="-4" y="28" width="8" height="8">
+    <ellipse/>
+   </object>
+   <object id="3" x="-6" y="20" width="8" height="8">
+    <ellipse/>
+   </object>
+   <object id="5" x="-6" y="36" width="8" height="8">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
