accc415751a8826a62e0389b8b7237786e20b5b3
IoriBranford <ioribranford@gmail.com>
2016-07-04 14:47:04 -0700

    Game music emu in Android.

---
+++ love-android/jni/game-music-emu-0.6.1/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := libgme
+FILE_LIST	:= $(wildcard $(LOCAL_PATH)/gme/*.cpp)
+LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
+
+include $(BUILD_SHARED_LIBRARY)
