e32b3c0a9b42cea1bf9121bfad8ea2144ff4588c
IoriBranford <ioribranford@gmail.com>
2017-04-24 15:18:25 -0700

    Reorganize forest objects tileset.

--- game/img/forest.tsx
+++ game/img/forest.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="forest" tilewidth="16" tileheight="16" tilecount="288" columns="12">
- <image source="forest.png" width="192" height="384"/>
+<tileset name="forest" tilewidth="16" tileheight="16" tilecount="32" columns="8">
+ <image source="forest.png" width="128" height="64"/>
 </tileset>
