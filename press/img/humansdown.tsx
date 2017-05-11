15a3b6b142e39c7d8f8a9cc9ddc519983ab8ebb2
IoriBranford <ioribranford@gmail.com>
2017-05-11 10:06:07 -0700

    Press graphics.

---
+++ press/img/humansdown.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="humansdown" tilewidth="24" tileheight="32" tilecount="48" columns="8">
+ <tileoffset x="-12" y="16"/>
+ <image source="humansdown.png" width="192" height="192"/>
+</tileset>
---
+++ press/img/imp2.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="imp2" tilewidth="64" tileheight="64" tilecount="3" columns="3">
+ <tileoffset x="-32" y="32"/>
+ <image source="imp2.png" width="192" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
